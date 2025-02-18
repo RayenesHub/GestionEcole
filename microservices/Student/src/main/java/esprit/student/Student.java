591063a5c680a84d45f6c4c53489d705ec742d6c
rihem09 <rihem.benaissa@esprit.tn>
2025-02-18 15:08:07 +0100

    Ajout initial du projet GestionEcole

---
+++ microservices/Student/src/main/java/esprit/student/Student.java
@@ -0,0 +1,55 @@
+package esprit.student;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+
+    public Student() {
+    }
+
+    public Student(String lastName, String firstName, String dateOfBirth) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
+
--- microservices/candidat/src/main/java/esprit/candidat/CandidatRespository.java
+++
@@ -1,6 +0,0 @@
-package esprit.candidat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidatRespository extends JpaRepository<Candidat, Integer> {
-}
