bb9f9f15e7d2ff0e46b95e2f825f5bbb1616f8a9
DinaBouchaddekh <159637984+DinaBouchaddekh@users.noreply.github.com>
2025-04-11 23:45:08 +0100

    ajout microservice event

--- backend/MaghrebiaExpert/src/main/java/esprit/maghrebiaexpert/entities/ClaimStatus.java
+++
@@ -1,5 +0,0 @@
-package esprit.maghrebiaexpert.entities;
-
-public enum ClaimStatus { PENDING, APPROVED, REJECTED
-}
-
--- backend/MaghrebiaExpert/src/main/java/esprit/maghrebiaexpert/repository/MechanicRepository.java
+++
@@ -1,9 +0,0 @@
-package esprit.maghrebiaexpert.repository;
-
-import esprit.maghrebiaexpert.entities.Mechanic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MechanicRepository extends JpaRepository<Mechanic, Long> {
-}
